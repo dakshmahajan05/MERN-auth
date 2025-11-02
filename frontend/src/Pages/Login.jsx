88062b2c8aa17b11a4f08ff46e51320b1dcb9355
Daksh Mahajan <mahajandaksh682@gmail.com>
2025-11-02 19:03:13 +0530

    fetching formdata

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -3,6 +3,14 @@ import { useState } from 'react'
 
 const  Login = () => {
     const [state,setstate] = useState('signup')
+    const [formdata,setformdata] = useState({
+        'email':'',
+        'fullname':'',
+        'password':''
+    })
+    const handlechange = (e)=>{
+        setformdata((prev)=>({...prev,[e.target.name]:e.target.value}))
+    }
   return (
     <div className='bg-gradient-to-br flex items-center justify-center from-blue-200 to-purple-400 min-h-screen'>
         <div className='sm:w-80 rounded-2xl flex flex-col items-center py-4 px-3 w-20 bg-gray-700  '>
@@ -12,15 +20,15 @@ const  Login = () => {
             <form action="">
                 <div className='flex flex-col items-center gap-3'>
                     {state==='signup' && (
-                <input className='bg-gray-600 mx-2 py-3 px-4 text-white  rounded-2xl' type="text" placeholder='Full Name' />
+                <input onChange={handlechange} name='fullname' value={formdata.fullname} className='bg-gray-600 mx-2 py-3 px-4 text-white  rounded-2xl' type="text" placeholder='Full Name' />
 
                     )}
-                <input className='bg-gray-600 mx-2  py-3 px-4 text-white rounded-2xl' type="text" placeholder='Email' />
-                <input className='bg-gray-600 mx-2  py-3 px-4 text-white rounded-2xl' type="text" placeholder='Password' />
+                <input onChange={handlechange} name='email' value={formdata.email} className='bg-gray-600 mx-2  py-3 px-4 text-white rounded-2xl' type="text" placeholder='Email' />
+                <input onChange={handlechange} name='password' value={formdata.password} className='bg-gray-600 mx-2  py-3 px-4 text-white rounded-2xl' type="password" placeholder='Password' />
                 </div>
                 <p className='ml-3 cursor-pointer mt-1 text-blue-500'>forgot password? </p>
 
-                <button className='w-full text-white font-bold bg-gradient-to-br from-blue-400 to-purple-600 rounded-2xl mb-1 mt-4 py-2 cursor-pointer hover:bg-blue-600'>{state==='signup'?"Sign Up":"Register"}</button>
+                <button className='w-full text-white font-bold bg-gradient-to-br from-blue-400 to-purple-600 rounded-2xl mb-1 mt-4 py-2 cursor-pointer hover:bg-blue-600'>{state==='signup'?"Sign Up":"Sign In"}</button>
                 {state==='signup'?(
 
                     <p className='mb-3'>already have an account? <span onClick={()=>setstate('login')} className='underline text-blue-600 cursor-pointer'>Login here</span> </p>
